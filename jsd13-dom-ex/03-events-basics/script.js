acb374c3797c210ba61eb382522e5d0af306a153
Wirakarn <wirakarn.ang@gmail.com>
2026-08-10 16:39:06 +0700

    dom events-basics completed

--- jsd13-dom-ex/03-events-basics/script.js
+++ jsd13-dom-ex/03-events-basics/script.js
@@ -2,17 +2,30 @@
 // Open index.html and work through these in order.
 
 // TODO 1: Select #box, #log, and #key-display.
-
+const box = document.querySelector("#box");
+const log = document.querySelector("#log");
+const keyDisplay = document.querySelector("#key-display");
 
 // TODO 2: Add a "click" listener on #box that sets log's textContent to
 // "Box clicked!". Inside the same listener, console.log() the event's
 // event.type and event.target (the event object is the first argument
-// your listener function receives).
-
+const boxClick = document.getElementById("box")
+boxClick.addEventListener("click", () => {
+    boxClick.textContent = "Box Clicked!"
+});
 
 // TODO 3: Add a "mouseover" listener on #box that adds the "hover" class
 // to it, and a "mouseout" listener that removes the "hover" class.
+box.addEventListener("mouseover", (event) => {
+    box.classList.add("hover");
+});
 
+box.addEventListener("mouseout", (event) => {
+    box.classList.remove("hover");
+});
 
 // TODO 4: Add a "keydown" listener on the whole document. Inside it, set
 // key-display's textContent to event.key (the key that was pressed).
+document.addEventListener("keydown", (event) =>{
+    keyDisplay.textContent = event.key;
+});
