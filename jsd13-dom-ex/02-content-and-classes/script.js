d51867e9bc63fb33414408f480198e0249b56611
Wirakarn <wirakarn.ang@gmail.com>
2026-08-10 14:45:54 +0700

    dom content-and-classes completed

--- jsd13-dom-ex/02-content-and-classes/script.js
+++ jsd13-dom-ex/02-content-and-classes/script.js
@@ -21,8 +21,14 @@ console.log(document.getElementById("card").getAttribute("class"));
 // TODO 4: Add the "highlight" class to #card using classList.add, then
 // console.log(card.className) to see it there. (You'll learn to do this in
 // response to a click in the next exercise, 03-events-basics.)
-
+const card = document.getElementById("card");
+card.classList.add("highlight");
+console.log(card.className);
 
 // TODO 5: #card starts with a data-visits="0" attribute. Read the current
 // value with card.dataset.visits, convert it to a number, add 1, write it
 // back to card.dataset.visits, and console.log() the new value.
+let visits = Number(card.dataset.visits);
+visits = visits + 1;
+card.dataset.visits = visits;
+console.log(card.dataset.visits);
